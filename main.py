bd0574d74a3cc6c1ce0501ee12e2a0a2040a6168
Mukhammad aziz <87309688+Inobiddinov@users.noreply.github.com>
2022-09-27 09:16:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import turtle
+
+
+#function
+aziz_turtle = turtle.Turtle()
+
+
+def square():
+ aziz_turtle.forward(100)
+ aziz_turtle.right(90)
+ aziz_turtle.forward(100)
+ aziz_turtle.right(90)
+ aziz_turtle.forward(100)
+ aziz_turtle.right(90)
+ aziz_turtle.forward(100)
+
+  
+
+
+square()
+square()
+square()
+square()
+aziz_turtle.forward(200)
+square()
+square()
+square()
+square()
+# Good job, azizbro Soon you will become a python programmer
+
+#if else function
+
